3563ea605cc4df6ecca020a2ac921b31158eea4a
HytonightYX <whosiyuan@qq.com>
2019-06-30 19:17:39 +0800

    feat: HttpException子类尝试

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -1,6 +1,6 @@
 const Router = require('koa-router')
 const router = new Router()
-const {HttpException} = require('../../../core/httpException')
+const {HttpException, ParameterException} = require('../../../core/httpException')
 
 router.get('/v1/classic/test', (ctx, next) => {
 	ctx.body = {test: 'v1 classic router ok'}
@@ -10,8 +10,8 @@ router.post('/v1/classic/error', (ctx, next) => {
 	// ctx.body = {test: 'v1 classic router ok'}
 	const query = ctx.request.query
 
-	if (true) {
-		throw new HttpException('出错啦!!', 10001, 400)
+	if (!query) {
+		throw new ParameterException()
 	}
 	ctx.body = {text: 'no error'}
 })
