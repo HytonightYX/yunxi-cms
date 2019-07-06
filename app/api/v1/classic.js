59124648082f2a1d12aa6d15ca9f0f9bf13b5aa8
HytonightYX <whosiyuan@qq.com>
2019-07-06 21:42:47 +0800

    feat: 获取指定index期刊的上/下一期

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -4,6 +4,7 @@ const {Auth} = require('../../../middlewares/auth')
 const {Flow} = require('../../models/flow')
 const {Art} = require('../../models/art')
 const {Favor} = require('../../models/favor')
+const {PositiveIntegerValidator} = require('../../validators/validator')
 
 /**
  * 测试接口
@@ -30,6 +31,55 @@ router.get('/latest', new Auth(7).m, async (ctx, next) => {
 	// art.dataValues.index = flow.index
 
 	// 使用内置方法
+	art.setDataValue('index', flow.index)
+	art.setDataValue('likeStatus', userLikeIt)
+	ctx.body = art
+})
+
+/**
+ * 获取指定index期刊的下一期
+ */
+router.get('/:index/next', new Auth().m, async ctx => {
+	const v = await new PositiveIntegerValidator().validate(ctx, {
+		id: 'index'
+	})
+
+	const index = v.get('path.index')
+
+	// 当前一期的下一期
+	const flow = await Flow.findOne({where: {index: index + 1}})
+
+	if (!flow) {
+		throw new global.errs.NotFound()
+	}
+
+	const art = await Art.getData(flow.artId, flow.type)
+	const userLikeIt = await Favor.userLikeIt(flow.artId, flow.type, ctx.auth.uid)
+
+	art.setDataValue('index', flow.index)
+	art.setDataValue('likeStatus', userLikeIt)
+	ctx.body = art
+})
+
+/**
+ * 获取指定index期刊的上一期
+ */
+router.get('/:index/previous', new Auth().m, async ctx => {
+	const v = await new PositiveIntegerValidator().validate(ctx, {
+		id: 'index'
+	})
+
+	const index = v.get('path.index')
+
+	const flow = await Flow.findOne({where: {index: index - 1}})
+
+	if (!flow) {
+		throw new global.errs.NotFound()
+	}
+
+	const art = await Art.getData(flow.artId, flow.type)
+	const userLikeIt = await Favor.userLikeIt(flow.artId, flow.type, ctx.auth.uid)
+
 	art.setDataValue('index', flow.index)
 	art.setDataValue('likeStatus', userLikeIt)
 	ctx.body = art
