47363b0f81a67ba250c3c7e93639cee1e2bc8663
HytonightYX <whosiyuan@qq.com>
2019-07-07 19:03:14 +0800

    feat: Art类型枚举类

--- app/lib/enum.js
+++ app/lib/enum.js
@@ -11,15 +11,33 @@ const LoginType = {
 	USER_EMAIL: 101,        // 邮箱登录
 	USER_MOBILE: 102,       // 手机号登录
 	ADMIN_EMAIL: 200,       // 管理员邮箱登录
+	isThisType
+}
 
-	isThisType(val) {
-		for (let key in this) {
-			if (this[key] === val) {
-				return true
-			}
+/**
+ * Art类型枚举类
+ * @type {{MUSIC: number, MOVIE: number, BOOK: number, SENTENCE: number}}
+ */
+const ArtType = {
+	MOVIE: 100,
+	MUSIC: 200,
+	SENTENCE: 300,
+	BOOK: 400,
+	isThisType
+}
+
+/**
+ * 判断val是否属于该类型
+ * @param val
+ * @returns {boolean}
+ */
+function isThisType(val) {
+	for (let key in this) {
+		if (this[key] === val) {
+			return true
 		}
-		return false
 	}
+	return false
 }
 
 module.exports = {
