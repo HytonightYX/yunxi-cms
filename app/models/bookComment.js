3682c8fe4bbc56952d569f0751d7adff7e49eb51
HytonightYX <whosiyuan@qq.com>
2019-07-09 12:35:36 +0800

    feat: 书籍评论 模型

---
+++ app/models/bookComment.js
@@ -0,0 +1,45 @@
+const {db} = require('../../core/db')
+const {Sequelize, Model} = require('sequelize')
+
+class Comment extends Model {
+	/**
+	 * 增加评论
+	 * @param bookId
+	 * @param content
+	 * @returns {Promise<Comment|this>}
+	 */
+	static async addComment(bookId, content) {
+		const comment = await Comment.findOne({
+			where: {
+				bookId: bookId,
+				content: content
+			}
+		})
+
+		if (!comment) {
+			return await Comment.create({
+				bookId: bookId,
+				content: content,
+				nums: 1
+			})
+		} else {
+			return await comment.increment('nums', {by: 1})
+		}
+	}
+}
+
+Comment.init({
+	bookId: Sequelize.INTEGER,
+	content: Sequelize.STRING(12),   // 短评限制12字符
+	nums: {
+		type: Sequelize.INTEGER,
+		defaultValue: 0
+	},
+}, {
+	sequelize: db,
+	tableName: 'comment'
+})
+
+module.exports = {
+	Comment
+}
